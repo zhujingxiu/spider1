dd46b7a4177cf4fac0b7501b7beee44720d9fc66
修 <zhujingxiu@qq.com>
2019-01-18 00:07:42 +0800

    Update bjzhufang.py
    modified crawl step

--- crawl58_spider/crawl58_spider/spiders/bjzhufang.py
+++ crawl58_spider/crawl58_spider/spiders/bjzhufang.py
@@ -25,37 +25,51 @@ class BjzhufangSpider(CrawlSpider):
         Rule(link, callback='parse_item', follow=True),
     )
 
-    def parse_item(self, response):
-        page_info = {}
-        script_text = response.xpath('//head/script[6]/text()').extract_first()
-        if script_text:
-            self.make_font(script_text)
+    # def parse_list(self, response):
+    #
+    #     renting_info = Crawl58SpiderItem()
+    #     image = li_item.xpath('./div[@class="img_list"]/a/img/@src').extract_first().strip()
+    #     print('image',image)
+    #     title = li_item.xpath('./div[@class="des"]/h2/a/text()').extract_first().strip()
+    #     print('title',title)
+    #     building = li_item.xpath('./div[@class="des"]/p[1]/text()').extract_first().strip()
+    #     print('building:',building)
+    #     address = li_item.xpath('./div[@class="des"]/p[2]/a[1]/text()').extract_first().strip()
+    #     print('address:',address)
+    #
+    #     money = li_item.xpath('./div[3]/div[2]/b/text()').extract_first().strip()
+    #     print('money',money)
+    #     info = {
+    #         'img': image,
+    #         'title': self.parse_crypt_text(title),
+    #         'building': self.parse_crypt_text(building),
+    #         'addr': self.parse_crypt_text(address),
+    #         'money': self.parse_crypt_text(money),
+    #     }
+    #     renting_info['title'] = self.parse_crypt_text(title)
+    #     renting_info['building'] = self.parse_crypt_text(building)
+    #     renting_info['address'] = self.parse_crypt_text(address)
+    #     renting_info['money'] = self.parse_crypt_text(money)
 
+    def parse_item(self, response):
         li_list = response.xpath('//div[@class="mainbox"]/div/div[@class="content"]/div[@class="listBox"]/ul/li')
         print(response.url, len(li_list))
         for i, li_item in enumerate(li_list, 1):
-            renting_info = Crawl58SpiderItem()
-            image = li_item.xpath('./div[@class="img_list"]/a/img/@src').extract_first().strip()
-            title = li_item.xpath('./div[@class="des"]/h2/a/text()').extract_first().strip()
-            building = li_item.xpath('./div[@class="des"]/p[@class="room"]/text()').extract_first().strip()
-            address = li_item.xpath('./div[@class="des"]/p[@class="add"]/text()').extract_first().strip()
-            source = li_item.xpath('./div[@class="des"]/div[@class="jjr"]/span[@class="jjr_par"]/text()').extract_first().strip()
-            money = li_item.xpath('./div[3]/div[2]/b/text()').extract_first().strip()
-            info = {
-                'img': image,
-                'title': self.parse_crypt_text(title),
-                'building': self.parse_crypt_text(building),
-                'addr': self.parse_crypt_text(address),
-                'source': source,
-                'money': self.parse_crypt_text(money),
-            }
-            renting_info['title'] = self.parse_crypt_text(title)
-            renting_info['building'] = self.parse_crypt_text(building)
-            renting_info['address'] = self.parse_crypt_text(address)
-            renting_info['source'] = self.parse_crypt_text(source)
-            renting_info['money'] = self.parse_crypt_text(money)
-            print(i, info)
-        return page_info
+
+            detail_url = li_item.xpath('./div[@class="des"]/h2/a/@href').extract_first().strip()
+            if detail_url:
+                yield scrapy.Request(url='https:'+detail_url, callback=self.parse_detail)
+
+    def parse_detail(self, response):
+        renting_info = Crawl58SpiderItem()
+        renting_info['crypt'] = ''
+        script_text = response.xpath('//head/script[1]/text()').extract_first()
+        if script_text:
+            renting_info['crypt'] = re.findall("src:url\('.*charset=utf-8;base64,(.*)'\) format", script_text)[0]
+        renting_info['title'] = response.xpath('//div[@class="main-wrap"]/div[1]/h1/text()').extract_first().strip()
+        renting_info['price'] = response.xpath('//div[@class="main-wrap"]/div[2]/div[2]/div[1]/div[1]/div/span[1]/b/text()').extract_first().strip()
+
+        yield renting_info
 
     def make_font(self, script_text):
         base64_text = re.findall("src:url\('.*charset=utf-8;base64,(.*)'\) format", script_text)[0]

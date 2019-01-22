f2ff567dee9fd52d248755d11048047cc71af871
zhujingxiu <zhujingxiu@gmail.com>
2019-01-22 18:46:15 +0800

    by ItemLoader

--- crawl58_spider/crawl58_spider/items.py
+++ crawl58_spider/crawl58_spider/items.py
@@ -6,16 +6,51 @@
 # https://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import MapCompose, TakeFirst
+
+
+class MyItemLoader(ItemLoader):
+    default_output_processor = TakeFirst()
+
+
+def parse_crypt(value):
+    import re
+    return re.findall("src:url\('.*charset=utf-8;base64,(.*)'\) format", value)[0]
+
+
+def return_value(value):
+    return value
+
+
+def json_images(value):
+    import json
+    return json.dumps(value)
 
 
 class Crawl58SpiderItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+
+    # title = scrapy.Field()
+    # cover = scrapy.Field()
+    # mode = scrapy.Field()
+    # house = scrapy.Field()
+    # crypt = scrapy.Field()
+    # price = scrapy.Field()
+    # payment = scrapy.Field()
+    # info = scrapy.Field()
+    # phone = scrapy.Field()
+    # address = scrapy.Field()
+    # position = scrapy.Field()
+    # source = scrapy.Field()
+    # images = scrapy.Field()
+    # proxy = scrapy.Field()
+
     title = scrapy.Field()
     cover = scrapy.Field()
     mode = scrapy.Field()
     house = scrapy.Field()
-    crypt = scrapy.Field()
+    crypt = scrapy.Field(input_processor=MapCompose(parse_crypt))
     price = scrapy.Field()
     payment = scrapy.Field()
     info = scrapy.Field()
@@ -23,5 +58,5 @@ class Crawl58SpiderItem(scrapy.Item):
     address = scrapy.Field()
     position = scrapy.Field()
     source = scrapy.Field()
-    images = scrapy.Field()
+    images = scrapy.Field(input_processor=MapCompose(json_images))
     proxy = scrapy.Field()
--- crawl58_spider/crawl58_spider/spiders/bjzhufang.py
+++ crawl58_spider/crawl58_spider/spiders/bjzhufang.py
@@ -5,7 +5,8 @@ import traceback
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from scrapy_redis.spiders import RedisCrawlSpider
-from crawl58_spider.items import Crawl58SpiderItem
+from crawl58_spider.items import Crawl58SpiderItem, MyItemLoader
+
 
 
 class BjzhufangSpider(RedisCrawlSpider):
@@ -31,46 +32,62 @@ class BjzhufangSpider(RedisCrawlSpider):
                 yield scrapy.Request(url='https:'+detail_url, callback=self.parse_detail)
 
     def parse_detail(self, response):
-        renting_info = Crawl58SpiderItem()
-        renting_info['proxy'] = response.meta['proxy']
-        renting_info['crypt'] = ''
-        try:
-            script_text = response.xpath('//head/script[1]/text()').extract_first()
-            if script_text:
-                renting_info['crypt'] = re.findall("src:url\('.*charset=utf-8;base64,(.*)'\) format", script_text)[0]
-            cover = response.css('#smainPic::attr(src)').extract_first()
-            renting_info['cover'] = cover.strip() if cover else ''
-            title = response.css('div.house-title h1::text').extract_first()
-            renting_info['title'] = title.strip() if title else ''
-            price = response.css('div.house-pay-way span:nth-child(1) b::text').extract_first()
-            renting_info['price'] = price.strip() if price else ''
-            payment = response.css('div.house-pay-way span:nth-child(2)::text').extract_first()
-            renting_info['payment'] = payment.strip() if payment else ''
-            mode = response.css('div.house-desc-item ul li:nth-child(1) span:nth-child(2)::text').extract_first()
-            renting_info['mode'] = mode.strip() if mode else ''
-            house = response.css('div.house-desc-item ul li:nth-child(2) span:nth-child(2)::text').extract_first()
-            renting_info['house'] = house.strip() if house else ''
-            position = response.css('div.house-desc-item ul li:nth-child(3) span:nth-child(2)::text').extract_first()
-            renting_info['position'] = position.strip() if position else ''
-            address = response.css('span.dz::text').extract_first()
-            renting_info['address'] = address.strip() if address else ''
-            phone = response.css('span.house-chat-txt::text').extract_first()
-            renting_info['phone'] = phone.strip() if phone else ''
-            source = response.css('p.agent-subgroup::text').extract_first()
-            renting_info['source'] = source.strip() if source else ''
-            info = response.css('ul.introduce-item li:nth-child(2) span:nth-child(2)::text').extract_first()
-            renting_info['info'] = info.strip() if info else ''
-            house_image = []
-            image_eles = response.css('#housePicList li')
-            if image_eles:
-                for img in image_eles:
-                    _path = img.css('img::attr(lazy_src)').extract_first()
-                    if not _path:
-                        continue
-                    house_image.append(_path)
-
-            renting_info['images'] = house_image
-        except Exception as e:
-            print(traceback.print_exc(), e)
+        _loader = MyItemLoader(item=Crawl58SpiderItem(), response=response)
+        _loader.add_value('proxy', response.meta['proxy'])
+        _loader.add_xpath('crypt', '//head/script[1]/text()')
+        _loader.add_css('cover', '#smainPic::attr(src)')
+        _loader.add_css('title', 'div.house-title h1::text')
+        _loader.add_css('price', 'div.house-pay-way span:nth-child(1) b::text')
+        _loader.add_css('payment', 'div.house-pay-way span:nth-child(2)::text')
+        _loader.add_css('mode', 'div.house-desc-item ul li:nth-child(1) span:nth-child(2)::text')
+        _loader.add_css('house', 'div.house-desc-item ul li:nth-child(2) span:nth-child(2)::text')
+        _loader.add_css('position', 'div.house-desc-item ul li:nth-child(3) span:nth-child(2)::text')
+        _loader.add_css('address', 'span.dz::text')
+        _loader.add_css('phone', 'span.house-chat-txt::text')
+        _loader.add_css('source', 'p.agent-subgroup::text')
+        _loader.add_css('info', 'ul.introduce-item li:nth-child(2) span:nth-child(2)::text')
+        _loader.add_css('images', '#housePicList li img::attr(lazy_src)')
+        renting_info = _loader.load_item()
+        # renting_info = Crawl58SpiderItem()
+        # renting_info['proxy'] = response.meta['proxy']
+        # renting_info['crypt'] = ''
+        # try:
+        #     script_text = response.xpath('//head/script[1]/text()').extract_first()
+        #     if script_text:
+        #         renting_info['crypt'] = re.findall("src:url\('.*charset=utf-8;base64,(.*)'\) format", script_text)[0]
+        #     cover = response.css('#smainPic::attr(src)').extract_first()
+        #     renting_info['cover'] = cover.strip() if cover else ''
+        #     title = response.css('div.house-title h1::text').extract_first()
+        #     renting_info['title'] = title.strip() if title else ''
+        #     price = response.css('div.house-pay-way span:nth-child(1) b::text').extract_first()
+        #     renting_info['price'] = price.strip() if price else ''
+        #     payment = response.css('div.house-pay-way span:nth-child(2)::text').extract_first()
+        #     renting_info['payment'] = payment.strip() if payment else ''
+        #     mode = response.css('div.house-desc-item ul li:nth-child(1) span:nth-child(2)::text').extract_first()
+        #     renting_info['mode'] = mode.strip() if mode else ''
+        #     house = response.css('div.house-desc-item ul li:nth-child(2) span:nth-child(2)::text').extract_first()
+        #     renting_info['house'] = house.strip() if house else ''
+        #     position = response.css('div.house-desc-item ul li:nth-child(3) span:nth-child(2)::text').extract_first()
+        #     renting_info['position'] = position.strip() if position else ''
+        #     address = response.css('span.dz::text').extract_first()
+        #     renting_info['address'] = address.strip() if address else ''
+        #     phone = response.css('span.house-chat-txt::text').extract_first()
+        #     renting_info['phone'] = phone.strip() if phone else ''
+        #     source = response.css('p.agent-subgroup::text').extract_first()
+        #     renting_info['source'] = source.strip() if source else ''
+        #     info = response.css('ul.introduce-item li:nth-child(2) span:nth-child(2)::text').extract_first()
+        #     renting_info['info'] = info.strip() if info else ''
+        #     house_image = []
+        #     image_eles = response.css('#housePicList li')
+        #     if image_eles:
+        #         for img in image_eles:
+        #             _path = img.css('img::attr(lazy_src)').extract_first()
+        #             if not _path:
+        #                 continue
+        #             house_image.append(_path)
+        #
+        #     renting_info['images'] = house_image
+        # except Exception as e:
+        #     print(traceback.print_exc(), e)
         yield renting_info
 

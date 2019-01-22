e6fe7e4d7cc09788b466f94092a4e449a0eb868a
zhujingxiu <zhujingxiu@gmail.com>
2019-01-22 15:53:55 +0800

    change craw58_spider with RedisCrawlSpider

--- crawl58_spider/crawl58_spider/middlewares.py
+++ crawl58_spider/crawl58_spider/middlewares.py
@@ -7,52 +7,63 @@
 
 import random
 from scrapy import signals
+from scrapy.contrib.downloadermiddleware.useragent import UserAgentMiddleware
 
 PROXIES = [
-    '119.180.177.118:8060',
-    '61.48.62.65:8060',
-    '195.235.204.178:8080',
-    '89.189.176.17:40990',
-    '176.117.216.122:61361',
-    '80.28.7.3:50866',
-    '124.41.211.86:6119',
+    ('http', '119.180.177.118:8060'),
+    ('http', '61.48.62.65:8060'),
+    ('https', '47.99.49.246:3128'),
+    ('https', '183.88.194.30:8080'),
+    ('https', '103.6.104.104:38898'),
+    ('http', '80.28.7.3:50866'),
 ]
 
-USER_AGENT = [
-    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
-    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
-    "Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
-    "Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)",
-    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)",
-    "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)",
-    "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)",
-    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.3 (Change: 287 c9dfb30)",
-    "Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6",
-    "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1",
-    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0",
-    "Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5",
-    "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
-    "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52",
-]
+USER_AGENT = ["Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 "
+              "(KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+              "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 "
+              "(KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
+              "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 "
+              "(KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6", "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 "
+                                                                     "(KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+              "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 "
+              "(KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 "
+                                                                    "(KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
+              "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 "
+              "(KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
+              "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 "
+              "(KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3", "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 "
+                                                                     "(KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+              "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 "
+              "(KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3", "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 "
+                                                                     "(KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+              "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 "
+              "(KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3", "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 "
+                                                                     "(KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+              "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 "
+              "(KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 "
+                                                                     "(KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+              "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 "
+              "(KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+              "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 "
+              "(KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+              "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 "
+              "(KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"]
 
 
 class MyHttpProxy(object):
 
     def process_request(self, request, spider):
-        item = random.choice(PROXIES)
-        print('当前代理：%s' % item)
-        request.meta['proxy'] = 'http://'+item
+        scheme, ip = random.choice(PROXIES)
+        print('当前代理：%s' % ip)
+        request.meta['proxy'] = '%s://%s' % (scheme, ip)
 
 
-class MyUserAgent(object):
+class MyUserAgent(UserAgentMiddleware):
 
     def process_request(self, request, spider):
-        item = random.choice(USER_AGENT)
-        print('当前ua：%s' % item)
-        request.meta['proxy'] = 'http://'+item
+        ua = random.choice(USER_AGENT)
+        request.headers.setdefault('User-Agent', ua)
+        print('当前ua：%s' % ua)
 
 
 class Crawl58SpiderSpiderMiddleware(object):
